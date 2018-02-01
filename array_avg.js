dcaf0d1fe0a81487fef053febcd801bfb50f714e
josh-z <d3V310p3r>
2018-02-01 18:51:27 -0500

    V2 2/1/2018
    somehow V1's version of array_avg.js got messed up, so in this version, i fixed it.

--- array_avg.js
+++ array_avg.js
@@ -1,7 +1,7 @@
 let my_arr = [1,2,3,4,5];
 
-function add(total, number){
-let sum=(total + number)
-return sum;}
-
-console.log(add(5,10));
+function array_avg (array){
+let sum = array.reduce((total, one_number) => total + one_number, 0);
+let avg=sum/array.length;
+return avg};
+console.log(array_avg(my_arr));
